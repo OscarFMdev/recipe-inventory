50740e88a6f8646e8bf9e2935d69d85f89fbfac1
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-02-07 16:30:46 -0600

    Add recipe controller and views

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,27 @@
+class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+  
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path, notice: 'Your recipe has been successfully deleted'
+  end
+end
