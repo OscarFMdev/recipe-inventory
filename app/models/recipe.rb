e6139a4b0f112a4aa3a99e86a97da57330bd1b24
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-02-07 16:42:33 -0600

    Fix linter errors

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,11 +2,10 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
-  
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :preparation_time, presence: true, length: { minimum: 5 }
   validates :cooking_time, presence: true, length: { minimum: 5 }
   validates :description, presence: true, length: { minimum: 10 }
   validates :public, presence: true
-
 end
