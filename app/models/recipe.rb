d634ffde419f0e3fae86991d64f7517f794d879c
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-02-07 13:36:56 -0600

    Remove default

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,5 +7,6 @@ class Recipe < ApplicationRecord
   validates :preparation_time, presence: true, length: { minimum: 5 }
   validates :cooking_time, presence: true, length: { minimum: 5 }
   validates :description, presence: true, length: { minimum: 10 }
-  validates :public, presence: true, default: false
+  validates :public, presence: true
+
 end
