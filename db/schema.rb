22cbd9976e2f7d965139a4e1ecb4018c20687987
Shedrack-Sunday <hannania002@gmail.com>
2023-02-07 18:47:10 +0100

    Add changes to migration structure

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_161146) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_07_162000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,8 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_161146) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "food_id", null: false
     t.bigint "recipe_id", null: false
+    t.bigint "food_id", null: false
     t.index ["food_id"], name: "index_recipe_foods_on_food_id"
     t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
