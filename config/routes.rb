c9cc81d82b69070dfc2a18858fa801e2a9193d46
Shedrack-Sunday <hannania002@gmail.com>
2023-02-08 01:42:18 +0100

    Add chnages to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  resources :foods
+  resources :users, only: [:index]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "foods/index"
 end
