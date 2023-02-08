f891cad12476ae6bbbde4a961de1db2a2c7add26
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-02-08 11:33:34 -0600

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -66,7 +66,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_08_151320) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
