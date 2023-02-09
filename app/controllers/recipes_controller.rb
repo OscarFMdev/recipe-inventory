e2592706c013c2a7456a188604fdce67f28ba48d
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-02-09 14:12:50 -0600

    Update recipe form and model

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -22,6 +22,9 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.user_id = current_user.id
+
+    @recipe.public = params.fetch(:recipe, {}).fetch(:public, false) == "true"
+
     respond_to do |format|
       if @recipe.save
         format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully created.' }
