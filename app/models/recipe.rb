0b3a4d8cf92462406d4afe97360bd911094d7020
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-02-09 14:13:02 -0600

    Update recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,5 +7,5 @@ class Recipe < ApplicationRecord
   validates :preparation_time, presence: true
   validates :cooking_time, presence: true
   validates :description, presence: true, length: { minimum: 10 }
-  validates :public, presence: true
+  validates :public, inclusion: { in: [true, false] }
 end
